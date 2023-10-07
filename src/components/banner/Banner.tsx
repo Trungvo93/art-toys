7362744ba60e4b47c6dcf5b481c7269113062aa8
Trungvo93 <thanhtrungvo1993@gmail.com>
2023-10-07 11:57:53 +0700

    .

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -5,13 +5,28 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-
+import { database } from '../../../firebase/firebaseConfig';
+import { ref, onValue } from 'firebase/database';
 import { Button, Image } from '@nextui-org/react';
+import { useState, useEffect } from 'react';
 type Banner = {
   id: string;
   src: string;
 };
 export default function BannerPage(props: { data: Banner[] | null }) {
+  const [data, setData] = useState<Banner[] | null>();
+  useEffect(() => {
+    const bannerRef = ref(database, 'banner');
+    onValue(bannerRef, (snapshot) => {
+      const data = snapshot.val();
+      if (!!data) {
+        setData(data);
+      } else {
+        console.log('Data not found');
+        setData(null);
+      }
+    });
+  }, []);
   return (
     <div className='p-4'>
       <Swiper
@@ -27,20 +42,20 @@ export default function BannerPage(props: { data: Banner[] | null }) {
             console.log('click ne');
           }}>
           <Image
-            src={props.data ? props.data[0].src : ''}
+            src={data ? data[0].src : ''}
             alt='NextUI hero Image'
           />
         </SwiperSlide>
         <SwiperSlide className='cursor-pointer'>
           <Image
-            src={props.data ? props.data[1].src : ''}
+            src={data ? data[1].src : ''}
             alt='NextUI hero Image'
           />
         </SwiperSlide>
       </Swiper>
       <Button
         onClick={() => {
-          console.log(props.data);
+          console.log(data);
         }}>
         click
       </Button>
