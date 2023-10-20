d82b655ac023d004cb93cf3df68daaa63dd83fc4
Trungvo93 <thanhtrungvo1993@gmail.com>
2023-10-20 22:41:59 +0700

    .

--- src/app/products/[productID]/page.tsx
+++ src/app/products/[productID]/page.tsx
@@ -7,8 +7,14 @@ import {
   orderByChild,
   equalTo,
   limitToFirst,
+  orderByValue,
+  orderByKey,
 } from 'firebase/database';
 import { database } from '../../../../firebase/firebaseConfig';
+import { Suspense } from 'react';
+
+import ProductItemPage from '@/components/products/product/page';
+import { isArray } from '@nextui-org/shared-utils';
 type Product = {
   id: string;
   title: string;
@@ -22,8 +28,6 @@ type Product = {
 };
 async function getProductItem() {
   const headersList = headers();
-  const domain = headersList.get('x-forwarded-host') || '';
-  const protocol = headersList.get('x-forwarded-proto') || '';
   const pathname = headersList.get('x-invoke-path') || '';
   const productID = pathname.slice(10);
   const productsRef = ref(database, 'products');
@@ -32,12 +36,15 @@ async function getProductItem() {
     orderByChild('id'),
     equalTo(productID)
   );
-  let result: Product[] | null = null;
+  let result: Product | null = null;
   await get(productQuery)
     .then((snapshot) => {
       if (snapshot.exists()) {
-        console.log(snapshot.val());
-        result = snapshot.val();
+        if (isArray(snapshot.val())) {
+          result = snapshot.val()?.filter((value: Product) => value)[0];
+        } else {
+          result = snapshot.val()[Object.keys(snapshot.val())[0]];
+        }
       } else {
         console.log('No data available');
       }
@@ -48,12 +55,12 @@ async function getProductItem() {
   return result;
 }
 export default async function ProductPage() {
-  const dataProduct: Product[] | null = await getProductItem();
-  console.log(dataProduct);
-
+  const dataProduct: Product | null = await getProductItem();
   return (
     <div>
-      <h1>Current pathname: {dataProduct && dataProduct[0]['title']}</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ProductItemPage data={dataProduct} />
+      </Suspense>
     </div>
   );
 }
--- src/components/products/product/page.tsx
+++ src/components/products/product/page.tsx
@@ -1,7 +1,96 @@
-export default function ProductItemPage() {
+'use client';
+import { Navigation, Thumbs, FreeMode } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+import Image from 'next/image';
+import { useState } from 'react';
+import { title } from 'process';
+type Product = {
+  id: string;
+  title: string;
+  price: number;
+  category: string;
+  brand: string;
+  description: string;
+  preview_url: string[];
+  detail: string[];
+  quantity: number;
+};
+export default function ProductItemPage(props: { data: Product | null }) {
+  const { data } = props;
+  const productItem = data;
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  // console.log(data);
   return (
-    <div>
-      <h1>Hello Page</h1>
+    <div className='mt-8 xl:mx-64 lg:mx-52  sm:mx-8 mx-4'>
+      <div className='grid md:grid-cols-2 grid-cols-1 gap-16  '>
+        <div>
+          <Swiper
+            modules={[FreeMode, Navigation, Thumbs]}
+            spaceBetween={50}
+            slidesPerView={1}
+            loop={true}
+            thumbs={{ swiper: thumbsSwiper }}
+            className=' overflow-hidden'>
+            {productItem?.preview_url.map((item, index) => (
+              <SwiperSlide
+                className='cursor-pointer  '
+                key={index}>
+                <div className='flex justify-center w-full'>
+                  <Image
+                    src={item}
+                    alt={productItem.title}
+                    width={2000}
+                    height={1000}
+                    className='w-auto h-auto object-cover '
+                  />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <Swiper
+            onSwiper={setThumbsSwiper}
+            loop={true}
+            spaceBetween={10}
+            slidesPerView={productItem?.preview_url.length}
+            freeMode={true}
+            navigation
+            watchSlidesProgress={true}
+            modules={[FreeMode, Navigation, Thumbs]}
+            className=' overflow-hidden'>
+            {productItem?.preview_url.map((item, index) => (
+              <SwiperSlide
+                className='cursor-pointer  '
+                key={index}>
+                <div className='flex justify-center w-full'>
+                  <Image
+                    src={item}
+                    alt={productItem.title}
+                    width={2000}
+                    height={1000}
+                    className='w-auto h-auto object-cover '
+                  />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div>
+          <p className='font-semibold text-xl'>{productItem?.title}</p>
+          <p className='text-default-red'>
+            {productItem?.price
+              .toString()
+              .replace(/[^-\d.]/g, '')
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            <span className='text-xs '>{productItem && 'đ'}</span>
+          </p>
+        </div>
+      </div>
+      <div></div>
     </div>
   );
 }
