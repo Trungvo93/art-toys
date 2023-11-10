01b81390cef17b271c5a5638975ed22d0bbbfa84
Trungvo93 <thanhtrungvo1993@gmail.com>
2023-11-10 09:44:04 +0700

    .

--- src/components/carts/CartsPage.tsx
+++ src/components/carts/CartsPage.tsx
@@ -1,11 +1,14 @@
 'use client';
 import { AppContext } from '@/context/contextConfig';
+import { DetailCart } from '@/lib/DefiningTypes';
 import Image from 'next/image';
 import { useState, useEffect, useContext } from 'react';
 
 export default function CartsPage() {
   const { state, dispatch } = useContext(AppContext);
-
+  const handleDecreaseQuantity = (item: DetailCart) => {
+    console.log(item);
+  };
   return (
     <div className='grid gap-4 mt-2'>
       {state.carts?.carts.map((item) => {
@@ -28,7 +31,13 @@ export default function CartsPage() {
                     {e.typeSku === 'signle' ? 'Hộp' : 'Set'}
                   </p>
                   <div className='flex gap-4 items-center'>
-                    <button className={` h-8 w-8 border `}>-</button>
+                    <button
+                      className={` h-8 w-8 border `}
+                      onClick={() => {
+                        handleDecreaseQuantity(item);
+                      }}>
+                      -
+                    </button>
                     <span>{e.count}</span>
                     <button className=' h-8 w-8 border '>+</button>
                   </div>
