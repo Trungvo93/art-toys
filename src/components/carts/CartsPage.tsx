5667f0103ee418e6436540a849f10471acce39e5
Trungvo93 <thanhtrungvo1993@gmail.com>
2023-11-10 11:37:05 +0700

    .

--- src/components/carts/CartsPage.tsx
+++ src/components/carts/CartsPage.tsx
@@ -1,14 +1,86 @@
 'use client';
 import { AppContext } from '@/context/contextConfig';
-import { DetailCart, QuantityDetailCart } from '@/lib/DefiningTypes';
+import { Cart, DetailCart, QuantityDetailCart } from '@/lib/DefiningTypes';
 import Image from 'next/image';
 import { useState, useEffect, useContext } from 'react';
-
+import {
+  ref,
+  get,
+  query,
+  orderByChild,
+  equalTo,
+  limitToFirst,
+  orderByValue,
+  orderByKey,
+  push,
+  update,
+  set,
+} from 'firebase/database';
+import { database } from '../../../firebase/firebaseConfig';
 export default function CartsPage() {
   const { state, dispatch } = useContext(AppContext);
-  const handleDecreaseQuantity = (e: QuantityDetailCart, index: number) => {
-    console.log(e);
-    console.log(index);
+  const handleDecreaseQuantity = async (
+    e: QuantityDetailCart,
+    index: number
+  ) => {
+    // const newData = { ...state.carts };
+    // if (newData.carts !== undefined) {
+    //   if (e.typeSku === 'signle') {
+    //     newData.carts[index].quantity[0].count = newData.carts[index]
+    //       .quantity[0].count--;
+    //   } else {
+    //     newData.carts[index].quantity[1].count = newData.carts[index]
+    //       .quantity[1].count--;
+    //   }
+    // }
+    const cartsRef = ref(database, 'carts');
+    await get(
+      query(cartsRef, orderByChild('userID'), equalTo(state.userProfile.uid))
+    )
+      .then((snapshot) => {
+        // If user have carts already
+        if (snapshot.exists()) {
+          const dataCart = Object.values(snapshot.val())[0] as Cart;
+          const keyCart = Object.keys(snapshot.val());
+
+          const newCart: Cart = { ...dataCart };
+          console.log('newCart: ', newCart);
+
+          if (e.typeSku === 'signle') {
+            newCart.carts[index].quantity[0].count = newCart.carts[index]
+              .quantity[0].count--;
+          } else {
+            newCart.carts[index].quantity[1].count = newCart.carts[index]
+              .quantity[1].count--;
+          }
+          // Put item to list carts of user
+          update(ref(database, `/carts/${keyCart}`), newCart);
+
+          //Update item to Badge Carts
+          let countItemCart = 0;
+          newCart?.carts.map((item: any) => {
+            if (item.quantity[0].count > 0) {
+              countItemCart++;
+            }
+            if (item.quantity[1].count > 0) {
+              countItemCart++;
+            }
+          });
+          dispatch({
+            type: 'CARTS_UPDATE_SUCCESS',
+            payload: newCart,
+          });
+          dispatch({
+            type: 'BADGE_UPDATE_SUCCESS',
+            payload: { counts: countItemCart },
+          });
+        } else {
+          console.log('Not item available');
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
   return (
     <div className='grid gap-4 mt-2'>
